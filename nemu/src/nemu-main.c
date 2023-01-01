f7a8fe6537dcfb71bc14efec3e3f3efb617e88b5
ysyx_22050850 闫俊杰 <21722037@bjtu.edu.cn>
2023-01-01 17:28:41 +0600

    vscode_commit_#1

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <common.h>
 
@@ -19,8 +19,11 @@ void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
+word_t expr(char *, bool *);
+static char main_expr_data[65540];
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
@@ -30,6 +33,24 @@ int main(int argc, char *argv[]) {
 
   /* Start engine. */
   engine_start();
-
-  return is_exit_status_bad();
+  /*
+  FILE *fp = fopen("/home/yjunj/ysyx-workbench/nemu/tools/gen-expr/build/input", "r");
+  assert(fp != NULL);
+  int cnt = 0;
+  word_t ans = 0;
+  while(fscanf(fp, "%u %s", &ans, main_expr_data)) {
+  bool exp_succ;
+  word_t tst_ans = expr(main_expr_data, &exp_succ);
+  if(ans == tst_ans)
+    printf("Success at point:%d\n",cnt++);
+  else {
+    printf("Failed at point:%d\n",cnt++);
+    break;
+  }
+  memset(main_expr_data, 0, sizeof(main_expr_data[0])*65540);
+  }
+  fclose(fp);
+  */
+  is_exit_status_bad();
+  return 0;
 }
